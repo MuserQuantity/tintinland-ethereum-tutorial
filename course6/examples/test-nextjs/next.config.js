883676a1a1069de7b05cad4fe89456b5675ad05d
Muser Quantity <muserquantity@gmail.com>
2023-05-05 15:23:30 +0800

    update

--- course6/examples/test-nextjs/next.config.js
+++ course6/examples/test-nextjs/next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} **/
 const nextConfig = {
   reactStrictMode: true,
+  // basePath: "/tintinland-ethereum-course6",
+  // publicRuntimeConfig: {
+  //   basePath: "/tintinland-ethereum-course6", //写入路径
+  // },
 };
 
 module.exports = nextConfig;
